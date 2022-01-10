1da593433c7f6b37b2e7c0942109587030fa0a88
Thierry Rocacher <trocache@insat.insa-toulouse.fr>
2022-01-10 21:13:03 +0100

    Synchro Sample OK, Validation détection 19 deg et 20 deg par 3 essais de suite. Réfléchir à un système propre pour sniffer tous les codes...

--- ProjCubeIDE/Core/Src/MyIT.c
+++ ProjCubeIDE/Core/Src/MyIT.c
@@ -33,8 +33,8 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) // front montant ou descendant GP
 
 NVIC->ICPR[1]|=(0x1<<8); // mise à 0 du pendig bit par mise à 1 !
 
-if (GPIO_Pin==GPIO_PIN_10) // front montant ou descendant input
-	{
+//if (GPIO_Pin==GPIO_PIN_10) // front montant ou descendant input
+	//{
 
 		if (HAL_GPIO_ReadPin(GPIOC,GPIO_PIN_10)==GPIO_PIN_SET) // front up
 		{
@@ -42,15 +42,15 @@ if (GPIO_Pin==GPIO_PIN_10) // front montant ou descendant input
 			{
 				Start=1;
 			}
-			FrontUp++;
 			if (FrontUp==FrontMax)
 			{
 				Start=0;
 			}
+			else FrontUp++;
 
 		}
-		//TIM2->CNT=0;
-	}
+		TIM2->CNT=0;
+//	}
 }
 
 void TIM2_IRQHandler(void) // Match de CCR, instant d'échantillonnage
@@ -61,16 +61,25 @@ void TIM2_IRQHandler(void) // Match de CCR, instant d'échantillonnage
 	// début pulse éch
 	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_11, GPIO_PIN_SET);
 
-	IndiceTab=NbEch%32;
+	IndiceTab=NbEch/32;
+
+	// insertion d'un 0 dans le 32bits courant
 	Tab_Ech[ IndiceTab]=Tab_Ech[ IndiceTab]<<1;
 
+	// insertion d'un 1 si l'input vaut 1
 	 if (HAL_GPIO_ReadPin(GPIOC,GPIO_PIN_10)==GPIO_PIN_SET)
 	 {
 		 Tab_Ech[ IndiceTab]=Tab_Ech[ IndiceTab]|1;
 	 }
-	 if (NbEch<384)
+
+
+	 if (NbEch<383)
 	 {
 		 NbEch++;
+	 }
+	 else
+	 {
+		 NbEch=NbEch;
 	 }
 
 	// fin pulse
