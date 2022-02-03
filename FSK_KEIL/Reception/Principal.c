45848a2bb2b458c9fa6ffb9d56cb2748a06ed601
Thierry Rocacher <trocache@insat.insa-toulouse.fr>
2022-02-03 21:34:52 +0100

    programme reception : trame d'envoie codée par ##### parsée, chaîne extraite, cad suppression caractères pourris d'entête ainsi que les #

---
+++ FSK_KEIL/Reception/Principal.c
@@ -0,0 +1,116 @@
+
+#include "clock.h"
+#include "API_ADC.h"
+#include "Timer_1234.h"
+#include "USART_rev2021.h"
+#include "FctDiverses.h"
+#include "API_FSK.h"
+#include "stdio.h"
+#include "GPIO.h"
+#include "LCD.h"
+
+char* Reponse;  						// déclaration d'un pointeur de caractère 
+ // réponse contient l'adresse du buffer qui contient
+														// la chaîne de caractères
+char pipo;
+#define HeaderCar '#'
+#define HeaderCarLen 5  // !!! impair !!!!!
+
+
+int LongRep;
+int HeaderCar_Count; // nbre de HeaderCar trouvé
+int Indice_HeaderCar[HeaderCarLen];  // indice dans la réponse où se trouve le dernier HeaderCar trouvé
+int ResultatParsing;
+
+#define HeaderCar '#'
+#define HeaderCarLen 5  // !!! impair !!!!!
+
+int ReponseParsing(void);
+
+
+int main (void)
+{
+  CLOCK_Configure();
+  Timer_1234_Init(TIM2, 1000000.0 );
+	Init_USART(USART2,9600, 0); // uart de l'xbee utilisé en réception
+	
+	lcd_init();
+	lcd_clear();
+	set_cursor(0, 0);
+	
+	
+  FSK_Init();
+	// TxCmde = 1
+	Port_IO_Set(GPIOB,8);
+//	Reponse=Get_String(USART1);
+	
+while(1)
+	{
+	if (Is_Str_Received(USART3)==1)
+		{
+			Reponse=Get_String(USART3); // jamais bloquant puisque un string est reçu !
+			LongRep=Read_Received_StrLen(USART3);
+			
+			ResultatParsing=ReponseParsing();
+			
+			if (ResultatParsing!=-1)
+			{
+			Reponse=Reponse+ResultatParsing;
+			lcd_clear();
+			set_cursor(0, 0);
+			lcd_print(Reponse);			
+		
+			}
+      pipo++;
+		}
+	}	
+}
+
+
+
+int ReponseParsing(void)
+{
+	int Sum;
+	int Moy;
+	int iloop;
+	int ReturnVal;
+	char* Rep;
+	
+	// parsing réponse
+	// forme de la châine : "xzeczcvAAAAMsg" : il faut détecter les AAAAA puis les supprimer et mettre à jour
+	// le pointeur de châine initialement en première position.
+	// Si AAAA n'est pas trouvé, abandon, retour "ùùùùErreur réception try again"
+	HeaderCar_Count=0;
+	Rep=Reponse;
+	for (iloop=0;iloop<HeaderCarLen;iloop++)	Indice_HeaderCar[iloop]=0;
+	Sum=0;
+	ReturnVal=-1;
+	
+	for (iloop=0;iloop<LongRep;iloop++)
+	{
+		if (*Rep==HeaderCar)
+		{
+			Indice_HeaderCar[HeaderCar_Count]=iloop; 
+			HeaderCar_Count++;
+			if (HeaderCar_Count==HeaderCarLen) break;
+					
+		}
+		Rep++; // incrémentation pointeur
+	}
+	if (HeaderCar_Count==HeaderCarLen) // 5 HeaderCar ont été trouvé
+	{
+		// calcul de la moyenne pour voir si les indices se suivent (principe du rami sur les suites)
+		for (iloop=0;iloop<HeaderCarLen;iloop++)	Sum=Sum+Indice_HeaderCar[iloop];
+		Moy=Sum/HeaderCarLen;
+		if (Moy==Indice_HeaderCar[(HeaderCarLen-1)/2]) // la moyenne vaut l'élément du milieu
+			// exemple : HeaderCarLen=5, et Indice_HeaderCar = {10 11 12 13 14}, Moy = 12 à comparer avec le 12 rang 3
+		{
+			ReturnVal=Indice_HeaderCar[HeaderCarLen-1] +1;
+		}
+		
+		
+	}		
+	
+	return ReturnVal;
+}
+
---
+++ FSK_KEIL/Reception/Service/API_FSK.c
@@ -0,0 +1,37 @@
+#include "GPIO.h"
+#include "FctDiverses.h"
+#include "USART_rev2021.h"
+
+#define USART_FSK USART3
+
+/*Son rôle se résume à initialiser la bonne USART et les pins de 
+commandes RxCmd et TxCmd en sortie pushpull. */
+void FSK_Init(void)
+{
+	Init_USART(USART_FSK,9600, 0); 
+	GPIO_Configure(GPIOB, 8, OUTPUT, OUTPUT_PPULL ); // RxCmde
+	GPIO_Configure(GPIOB, 9, OUTPUT, OUTPUT_PPULL ); // TxCmde
+}
+
+
+void FSK_Send_Str(char *Msg)
+/*La fonction envoie une chaîne de caractère dont l'adresse 
+du premier caractère sera passé dans le paramètre Msg (pointeur de caractère).*/ 
+{
+	// TxCmde = 1
+	Port_IO_Set(GPIOB,9);
+	Delay_x_ms(10);
+	Put_String(USART_FSK,Msg);
+	Delay_x_ms(3);
+	GPIO_Clear(GPIOB,9);
+}
+
+
+char * FSK_Get_Str(void);
+
+/*La fonction utilise directement la fonction 
+char * Get_String(USART_TypeDef *USART) de la lib USART_rev. 
+Notez que par défaut, une chaîne de caractères est considérée 
+complète losque le caractère <CR> est reçu. Ce caractère de fin 
+de chaîne est modifiable dans la fichier USART_User_Conf_2018.h.*/
+
